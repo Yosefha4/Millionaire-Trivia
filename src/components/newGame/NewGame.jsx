50e9e5b4b2c6d646ff096f4b279bfc35138795fa
Yosefha4 <73281960+Yosefha4@users.noreply.github.com>
2022-10-13 16:42:51 +0300

    Create NewGame.jsx

---
+++ src/components/newGame/NewGame.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./newGame.css";
+
+const NewGame = () => {
+  return (
+    <div className="container">
+      <div className="newG">
+        <h1>Congratulations, You Won !</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NewGame;
