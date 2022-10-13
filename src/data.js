d4e1398ce5708aa12c38cd142d920c26d4b8f8bd
Yosefha4 <73281960+Yosefha4@users.noreply.github.com>
2022-10-13 16:48:22 +0300

    Create data.js
    Add data file ( ques & ans )

---
+++ src/data.js
@@ -0,0 +1,339 @@
+
+
+
+const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "How many states are there in the United States?",
+      answers: [
+        {
+          text: "50",
+          correct: true,
+        },
+        {
+          text: "52",
+          correct: false,
+        },
+        {
+          text: "54",
+          correct: false,
+        },
+        {
+          text: "58",
+          correct: false,
+        },
+      ],
+    },
+
+    {
+      id: 5,
+      question: " Who, in 1903, was the first woman to win a Nobel Prize? ",
+      answers: [
+        {
+          text: "condoleezza rice",
+          correct: false,
+        },
+        {
+          text: "ruth bader ginsburg",
+          correct: false,
+        },
+        {
+          text: "Marie Curie",
+          correct: true,
+        },
+        {
+          text: "Rosalyn Sussman Yalow",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "What year did the Berlin wall fall? ",
+      answers: [
+        {
+          text: "1971",
+          correct: false,
+        },
+        {
+          text: "1898",
+          correct: false,
+        },
+        {
+          text: "1949",
+          correct: false,
+        },
+        {
+          text: "1989",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "What cuontry hosted the 2014 Winter Olympics? ",
+      answers: [
+        {
+          text: "Russia",
+          correct: true,
+        },
+        {
+          text: "China",
+          correct: false,
+        },
+        {
+          text: "Brazil",
+          correct: false,
+        },
+        {
+          text: "England",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: " What year was the first iPhone released? ",
+      answers: [
+        {
+          text: "1999",
+          correct: false,
+        },
+        {
+          text: "2004",
+          correct: false,
+        },
+        {
+          text: "1997",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: " Where did the band ABBA form? ",
+      answers: [
+        {
+          text: "India",
+          correct: false,
+        },
+        {
+          text: "Sweden",
+          correct: true,
+        },
+        {
+          text: "Switzerland",
+          correct: false,
+        },
+        {
+          text: "Denmark",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: " Who was the first Black actress to win an Oscar? ",
+      answers: [
+        {
+          text: "Rupert Crosse",
+          correct: false,
+        },
+        {
+          text: "Dorothy Dandridge",
+          correct: false,
+        },
+        {
+          text: "Chadwick Boseman",
+          correct: false,
+        },
+        {
+          text: "Hattie McDaniel",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "What is the capital city of Sweden? ",
+      answers: [
+        {
+          text: "Madrid",
+          correct: false,
+        },
+        {
+          text: "Stockholm",
+          correct: true,
+        },
+        {
+          text: "Malmo",
+          correct: false,
+        },
+        {
+          text: "Glasgow",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Which country invented tea? ",
+      answers: [
+        {
+          text: "Japan",
+          correct: false,
+        },
+        {
+          text: "England",
+          correct: false,
+        },
+        {
+          text: "China",
+          correct: true,
+        },
+        {
+          text: "Scotland",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "What year did the Titanic movie come out?",
+      answers: [
+        {
+          text: "1994",
+          correct: false,
+        },
+        {
+          text: "1989",
+          correct: false,
+        },
+        {
+          text: "2002",
+          correct: false,
+        },
+        {
+          text: "1997",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "The Statue of Liberty was given to the US by which country?",
+      answers: [
+        {
+          text: "France",
+          correct: true,
+        },
+        {
+          text: "Spain",
+          correct: false,
+        },
+        {
+          text: "England",
+          correct: false,
+        },
+        {
+          text: "Russia",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question:
+        "When Michael Jordan played for the Chicago Bulls, how many NBA Championships did he win?",
+      answers: [
+        {
+          text: "3",
+          correct: false,
+        },
+        {
+          text: "4",
+          correct: false,
+        },
+        {
+          text: "5",
+          correct: false,
+        },
+        {
+          text: "6",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  export default data;
